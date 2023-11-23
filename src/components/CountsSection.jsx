1c259ef9d5090152149cebcea69c1f832e6407b9
LeandroMonteros <69606577+LeandroMonteros@users.noreply.github.com>
2023-11-22 22:49:48 -0300

    anda count

--- src/components/CountsSection.jsx
+++ src/components/CountsSection.jsx
@@ -1,36 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import imgcounts from '../img/counts-img.svg';
+import { Clock } from 'react-bootstrap-icons';
+import { EmojiSmile } from 'react-bootstrap-icons';
+import { JournalRichtext } from 'react-bootstrap-icons';
+import { Award } from 'react-bootstrap-icons';
+import PureCounter from "@srexi/purecounterjs";
 
 function CountsSection() {
+    useEffect(() => {
+        new PureCounter();
+    }, []);
+    
+
     return (
         <section id="counts" className="counts">
             <div className="container">
                 <div className="row">
-                    <div className="col-xl-5 d-flex justify-content-center align-items-stretch justify-content-xl-start image" data-aos="fade-right" data-aos-delay="150">
+                    <div className="col-xl-5 d-flex justify-content-center align-items-stretch justify-content-xl-start image" data-aos="fade-right" data-aos-delay="1">
                         <img className="img-fluid" src={imgcounts} alt=""></img>
                     </div>
-                    <div className="col-xl-7 d-flex align-items-stretch pt-4 pt-xl-0" data-aos="fade-left" data-aos-delay="300">
+                    <div className="col-xl-7 d-flex align-items-stretch pt-4 pt-xl-0" data-aos="fade-left" data-aos-delay="1">
                         <div className="d-flex flex-column justify-content-center content">
                             <div className="row">
                                 <div className="col-md-6 d-md-flex align-items-md-stretch">
-                                    <div className="count-box"><i className="bi bi-emoji-smile"></i><span className="purecounter" data-purecounter-start="0" data-purecounter-end="65" data-purecounter-duration="1"></span>
+                                    <div className="count-box"><EmojiSmile></EmojiSmile><span className="purecounter" data-purecounter-start="0" data-purecounter-end="65" data-purecounter-duration="1"></span>
                                         <p><strong>Clientes felices</strong> La satisfacción de nuestros clientes es el motor de nuestra pasión.</p>
                                     </div>
                                 </div>
                                 <div className="col-md-6 d-md-flex align-items-md-stretch">
-                                    <div className="count-box"><i className="bi bi-journal-richtext"></i><span className="purecounter" data-purecounter-start="0" data-purecounter-end="85" data-purecounter-duration="1"></span>
-                                        <p><strong>Proyectos</strong> Cada proyecto es un lienzo donde plasmamos su éxito digital.</p>
+                                    <div className="count-box"><JournalRichtext /><span className="purecounter" data-purecounter-start="0" data-purecounter-end="85" data-purecounter-duration="1"></span>
+                                        <p><strong>Proyectos</strong> Cada proyecto es un lienzo donde plasmamos su éxito digital.</p>
                                     </div>
-                                </div>
-                                <div class="col-md-6 d-md-flex align-items-md-stretch">
-                                    <div className="count-box"><i className="bi bi-clock"></i><span className="purecounter" data-purecounter-start="0" data-purecounter-end="18" data-purecounter-duration="1"></span>
-                                        <p><strong>Años de experiencia</strong> Más de una década liderando el camino en el mundo digital.</p>
-                                    </div>
-
                                 </div>
                                 <div className="col-md-6 d-md-flex align-items-md-stretch">
-                                    <div className="count-box"><i class="bi bi-award"></i><span className="purecounter" data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1"></span>
-                                        <p><strong>Premios</strong> &quot;Reconocidos por nuestra excelencia, nuestros premios son testimonio de dedicación.</p>
+                                    <div className="count-box"><Clock /><span className="purecounter" data-purecounter-start="0" data-purecounter-end="18" data-purecounter-duration="1"></span>
+                                        <p><strong>Años de experiencia</strong> Más de una década liderando el camino en el mundo digital.</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 d-md-flex align-items-md-stretch">
+                                    <div className="count-box"><Award /><span className="purecounter" data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1"></span>
+                                        <p><strong>Premios</strong> "Reconocidos por nuestra excelencia, nuestros premios son testimonio de dedicación.</p>
                                     </div>
                                 </div>
                             </div>
