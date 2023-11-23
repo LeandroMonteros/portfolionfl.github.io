7e4bbf35179f6269bdea4523adf88df9ef8ab230
LeandroMonteros <69606577+LeandroMonteros@users.noreply.github.com>
2023-11-22 23:32:45 -0300

    l

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -1,17 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import image1 from '../img/portfolio/portafolio-1.jpeg';
+import image2 from '../img/portfolio/portafolio-2.jpeg';
+import image3 from '../img/portfolio/portafolio-3.jpeg';
 
 function PortfolioSection() {
+  const [filter, setFilter] = useState('*');
+
+  const handleFilterClick = (value) => {
+    setFilter(value);
+    console.log(value);
+  };
+
   return (
     <section id="portfolio" className="portfolio">
-        <div className="container">
-            <div data-aos="fade-up" className="section-title">
-                <h2>Portfolio</h2>
-                <p>Nuestro portafolio demuestra la calidad de nuestro trabajo</p>
-            </div>
-            <div className="row" data-aos="fade-up" data-aos-delay="200">
-                {/* ... Contenido de la sección "Portfolio" ... */}
-            </div>
+      <div className="container">
+        <div data-aos="fade-up" className="section-title">
+          <h2>Portfolio</h2>
+          <p>Nuestro portafolio demuestra la calidad de nuestro trabajo</p>
         </div>
+        <div className="row" data-aos="fade-up" data-aos-delay="200">
+          <div className="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li onClick={() => handleFilterClick('*')} className={filter === '*' ? 'filter-active' : ''}>Todos</li>
+              <li onClick={() => handleFilterClick('.filter-app')} className={filter === '.filter-app' ? 'filter-active' : ''}>Hoteleria</li>
+              <li onClick={() => handleFilterClick('.filter-card')} className={filter === '.filter-card' ? 'filter-active' : ''}>Construccion</li>
+            </ul>
+          </div>
+        </div>
+        <div className="row portfolio-container" data-aos="fade-up" data-aos-delay="400">
+          <div className={`col-md-6 col-lg-4 portfolio-item ${filter}`}>
+            <div className="portfolio-wrap">
+              <img className="img-fluid" src={image1} alt="Hotel Serenidad" />
+              <div className="portfolio-info">
+                <h4>Hotel Serenidad</h4>
+                <p>Hoteleria</p>
+                <div className="portfolio-links">
+                  <a href={image1} data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 1"></a>
+                  <a href="/portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`col-md-6 col-lg-4 portfolio-item ${filter}`}>
+            <div className="portfolio-wrap">
+              <img className="img-fluid" src={image2} alt="Grupo SIA" />
+              <div className="portfolio-info">
+                <h4>Grupo SIA</h4>
+                <p>Construccion</p>
+                <div className="portfolio-links">
+                  <a href={image2} data-gallery="portfolioGallery" className="portfolio-lightbox" title="Web 3"></a>
+                  <a href="/portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`col-md-6 col-lg-4 portfolio-item ${filter}`}>
+            <div className="portfolio-wrap">
+              <img className="img-fluid" src={image3} alt="Stars Hotels" />
+              <div className="portfolio-info">
+                <h4>Stars Hotels</h4>
+                <p>Hoteleria</p>
+                <div className="portfolio-links">
+                  <a href={image3} data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 2"></a>
+                  <a href="/portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
