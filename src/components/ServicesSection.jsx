a6db6ee1acb8f94882c1b75db76aa5807ccf90ed
LeandroMonteros <69606577+LeandroMonteros@users.noreply.github.com>
2023-11-22 23:18:00 -0300

    Services workin

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
+import { GlobeAmericas, Speedometer,FileBarGraph, Cursor } from 'react-bootstrap-icons';
 
 function ServicesSection() {
   return (
     <section id="services" className="services">
-        <div className="container">
-            <div data-aos="fade-up" className="section-title">
-                <h2>servicios</h2>
-            </div>
-            <div className="row">
-                {/* ... Contenido de la sección "Servicios" ... */}
-            </div>
+      <div className="container">
+        <div data-aos="fade-up" className="section-title">
+          <h2>servicios</h2>
         </div>
+        <div className="row">
+          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div data-aos="fade-up" data-aos-delay={400} className="icon-box">
+              <div className="icon"><GlobeAmericas /></div>
+              <h4 className="title"><a href>Diseño y Desarrollo Web</a></h4>
+              <p className="description">Creación de sitios web atractivos, funcionales y optimizados para una experiencia óptima.</p>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div data-aos="fade-up" data-aos-delay={300} className="icon-box">
+              <div className="icon"><Speedometer /></div>
+              <h4 className="title"><a href>Publicidad Digital</a></h4>
+              <p className="description">Campañas en línea estratégicas para aumentar el alcance y la conversión de clientes potenciales.</p>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div data-aos="fade-up" data-aos-delay={200} className="icon-box">
+              <div className="icon"><FileBarGraph /></div>
+              <h4 className="title"><a href>Gestión de Redes Sociales</a></h4>
+              <p className="description">Estrategias y contenido para impulsar la presencia y compromiso en plataformas sociales.</p>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div data-aos="fade-up" data-aos-delay={100} className="icon-box">
+              <div className="icon"><Cursor /></div>
+              <h4 className="title"><a href>Optimización SEO</a></h4>
+              <p className="description">Mejora del posicionamiento en buscadores para aumentar la visibilidad online.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
