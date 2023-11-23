e55d731bdb464d82e294f3a47d1aca8a7e151f78
LeandroMonteros <69606577+LeandroMonteros@users.noreply.github.com>
2023-11-23 12:19:11 -0300

    mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import logo from '../img/logo solo.png';
+import { List, XLg } from 'react-bootstrap-icons';
 
 function Header() {
   const [scrolled, setScrolled] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,22 +20,25 @@ function Header() {
       window.removeEventListener('scroll', handleScroll);
     };
   }, [scrolled]);
+
   return (
     <header className={`d-flex align-items-center fixed-top ${scrolled ? 'header-scrolled' : ''}`} id="header">
       <div className="container d-flex justify-content-between align-items-center">
         <div className="logo">
           <img src={logo} alt="Logo" />
         </div>
-        <nav id="navbar" className="navbar">
+        <nav id="navbar" className={`navbar ${mobileMenuOpen ? 'navbar-mobile' : ''}`}>
           <ul>
-            <li><a className="nav-link scrollto active" href="#hero">Inicio</a></li>
-            <li><a className="nav-link scrollto" href="#about">Nosotros</a></li>
-            <li><a className="nav-link scrollto" href="#services">Servicios</a></li>
-            <li><a className="nav-link scrollto" href="#portfolio">Portafolio</a></li>
-            <li><a className="nav-link scrollto" href="#pricing">Paquetes</a></li>
-            <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
+            <li><a className="nav-link scrollto active" href="#hero" onClick={() => setMobileMenuOpen(false)}>Inicio</a></li>
+            <li><a className="nav-link scrollto" href="#about" onClick={() => setMobileMenuOpen(false)}>Nosotros</a></li>
+            <li><a className="nav-link scrollto" href="#services" onClick={() => setMobileMenuOpen(false)}>Servicios</a></li>
+            <li><a className="nav-link scrollto" href="#portfolio" onClick={() => setMobileMenuOpen(false)}>Portafolio</a></li>
+            <li><a className="nav-link scrollto" href="#pricing" onClick={() => setMobileMenuOpen(false)}>Paquetes</a></li>
+            <li><a className="nav-link scrollto" href="#contact" onClick={() => setMobileMenuOpen(false)}>Contact</a></li>
           </ul>
-          <i className="bi bi-list mobile-nav-toggle"></i>
+          <i className={`bi mobile-nav-toggle ${mobileMenuOpen ? 'bi-x' : 'bi-list'}`} onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+            {mobileMenuOpen ? <XLg /> : <List />}
+          </i>
         </nav>
       </div>
     </header>
