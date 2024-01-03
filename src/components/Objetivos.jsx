e8483e2993c02e3baff34799d5a5a03ad241e4cb
LeandroMonteros <69606577+LeandroMonteros@users.noreply.github.com>
2024-01-03 12:04:33 -0300

    a

--- src/components/Objetivos.jsx
+++ src/components/Objetivos.jsx
@@ -2,16 +2,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import mejor from '../img/mejor-visibilidad.png';
+import { ArrowDownCircleFill } from 'react-bootstrap-icons';
 
-const Objetivos = ({ title, description, bgColor }) => {
-    const sectionStyle = {
-        background: `linear-gradient(to right, ${bgColor}, #29BDF4)`,
+const Objetivos = ({ title, description, border }) => {
+    const style = {
+        borderRadius: border,
     };
 
     return (
-        <div className="container"  style={sectionStyle}>
-            <div className="objetivos">
-                <h3>{title}</h3>
+        <div className="container">
+            <div className="objetivos" style={style}>
+                <div className='title'>
+                    <h3>{title}</h3>
+                    <div className='icon animated'><ArrowDownCircleFill /></div>
+                </div>
                 <div className="contente">
                     <img src={mejor} className="img-fluide" alt="Mejor visibilidad" />
                     <div className='parraf'>
@@ -27,7 +31,7 @@ const Objetivos = ({ title, description, bgColor }) => {
 Objetivos.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
-    bgColor: PropTypes.string.isRequired,
+    border: PropTypes.string.isRequired,
 };
 
 export default Objetivos;
--- src/components/OjectivsSection.jsx
+++ src/components/OjectivsSection.jsx
@@ -5,28 +5,26 @@ const ObjectivesSection = () => {
     const objectivesText1 = {
         title: "Mejorar la visibilidad de tu negocio en internet",
         description: "Un sitio web hermoso es tan valioso como una tarjeta de presentación en los años 70 y 80. Es el primer punto de contacto y, como dice la expresión, la primera impresión es, de hecho, lo que queda. Un mal diseño web y una experiencia negativa pueden acabar alejando a un cliente potencial.",
-        bgColor: "#000056",
+        border: "10px 10px 0px 0px",
       };
       const objectivesText2 = {
         title: "Identidad Propia",
         description: "Una marca sólida es aquella que los clientes recuerdan cuando vuelven a la tienda en busca de un producto determinado. Es mucho más que un refresco, es una Coca-Cola. Su marca es tan potente que la competencia suele usar logotipos y envases similares en rojo y blanco para intentar robarle consumidores.",
-        bgColor: "#220056",
       };
       const objectivesText3 = {
         title: "Medición de resultados",
         description: "Nada define más el éxito del marketing digital que su capacidad de medir, en tiempo real, cada resultado. Cada paso dado dentro de una estrategia online puede ser acompañado y analizado.¿Has enviado un email promocional? Mide la tasa de apertura y de conversión y descubre cuántas personas lo abrieron y cuántas adquirieron la promoción.",
-        bgColor: "#5F0A56",
       };
       const objectivesText4 = {
         title: "Fortalecer la marca ",
         description: "Un sitio web bien desarrollado también se convierte en una forma de fortalecer tu marca. El término conocido como Brand Awareness se mejora al brindar a los visitantes una experiencia positiva en tus páginas. Como decíamos, la credibilidad se acaba reforzando con una página de calidad y esto se refleja en la fuerza que tiene tu marca en el mercado.",
-        bgColor: "#861456",
+        border: "0px 0px 10px 10px",
       };
 
     return (
         <section id='Objetive'>
             <div className='listObjetives'>
-                <div className='box'>
+                <div className='boxy'>
                 <Objetivo {...objectivesText1} />
             <Objetivo {...objectivesText2} />
             <Objetivo {...objectivesText3} />
